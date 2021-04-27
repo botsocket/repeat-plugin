8bae67d1d26d54070da49ba146cd6ecadd59f418
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2021-04-28 00:51:01 +0200

    Update plugin & update Jade

--- index.js
+++ index.js
@@ -11,41 +11,38 @@ module.exports = {
             alias: ['say'],
             args: ['text'],
             flags: ['channel'],
-            data: {
-                validation: {
-                    args: Jade.object({
-                        text: Jade.string().required(),
-                    }),
-                    failAction: 'error',
-                },
-                handler: async (message, { args: { text }, flags: { channel } }) => {
+            validate: {
+                args: Jade.obj({
+                    text: Jade.string().required(),
+                }),
+            },
+            handler: async (message, { args: { text }, flags: { channel } }) => {
 
-                    if (!message.member.hasPermission('ADMINISTRATOR')) {
-                        return message.reply(`You are missing the \`ADMINISTRATOR\` permission.`);
+                if (!message.member.hasPermission('ADMINISTRATOR')) {
+                    return message.reply(`You are missing the \`ADMINISTRATOR\` permission.`);
+                }
+
+                let c = message.channel;
+
+                if (channel) {
+
+                    if (message.client.channels.cache.has(channel)) {
+
+                        c = message.client.channels.cache.get(channel);
                     }
+                    else {
 
-                    let c = message.channel;
-
-                    if (channel) {
-
-                        if (message.client.channels.cache.has(channel)) {
-
-                            c = message.client.channels.cache.get(channel);
-                        }
-                        else {
-
-                            const match = channel.match(/<#(\d{17,19})>/);
-                            if (message.client.channels.cache.has(match[1])) {
-                                c = message.client.channels.cache.get(match[1]);
-                            }
+                        const match = channel.match(/<#(\d{17,19})>/);
+                        if (message.client.channels.cache.has(match[1])) {
+                            c = message.client.channels.cache.get(match[1]);
                         }
                     }
+                }
 
-                    try {
-                        await c.send(text);
-                    }
-                    catch {}
-                },
+                try {
+                    await c.send(text);
+                }
+                catch {}
             },
         });
     },
